94622f57ef6fd007857670883f5a03f7656e0cde
pranavkavade16 <pranavkavade69739@gmail.com>
2026-05-31 11:30:18 +0530

    adding image model

--- models/album.model.js
+++ models/album.model.js
@@ -12,13 +12,12 @@ const albumSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      trim: true,
+     
     },
 
     description: {
       type: String,
       default: "",
-      trim: true,
     },
 
     ownerId: {
---
+++ models/image.model.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false },
+);
+
+const imageSchema = new mongoose.Schema(
+  {
+    imageId: {
+      type: String,
+      default: uuidv4,
+      unique: true,
+    },
+
+    albumId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Album",
+      required: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    tags: [
+      {
+        type: String,
+      },
+    ],
+
+    person: {
+      type: String,
+    },
+
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+
+    comments: [commentSchema],
+
+    size: {
+      type: Number,
+      required: true,
+    },
+
+    uploadedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Image", imageSchema);
