4a25a7999302ea912185bad0204615b1f1ba1a55
pranavkavade16 <pranavkavade69739@gmail.com>
2026-05-31 18:30:12 +0530

    added album validator

---
+++ modules/albums/album.validator.js
@@ -0,0 +1,33 @@
+const validateCreateAlbum = (req, res, next) => {
+  const { name, description } = req.body;
+
+  errors = [];
+
+  if (!name || typeof name !== "string") {
+    errors.push("Album name is required.");
+  }
+
+  if (name && name.trim().length === 0) {
+    errors.push("Album name cannot be empty.");
+  }
+
+  if (name && name.length > 100) {
+    errors.push("Album name cannot exceed 100 characters");
+  }
+
+  if (description && typeof description !== "string") {
+    errors.push("Description must be a string");
+  }
+
+  if (description && description.length > 500) {
+    errors.push("Description cannot exceed 500 characters");
+  }
+
+  if (errors.length > 0) {
+    return res.status(400).json({ success: false, errors });
+  }
+
+  next();
+};
+
+module.exports = { validateCreateAlbum };
