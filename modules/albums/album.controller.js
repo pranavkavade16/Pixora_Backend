6237e1aafc989130f217eb757a858c3e098ccf74
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-02 23:38:08 +0530

    added update description api controller

--- modules/albums/album.controller.js
+++ modules/albums/album.controller.js
@@ -9,3 +9,20 @@ exports.createAlbum = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.updateAlbum = async (req, res, next) => {
+  try {
+    const album = await albumService.updateAlbum(
+      req.params.albumId,
+      req.user.userId,
+      req.body.description,
+    );
+
+    return res.status(200).json({
+      success: true,
+      data: album,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
