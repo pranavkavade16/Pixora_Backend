5b56a39bd477608003104bb5b258fe7357b257e1
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-02 23:43:15 +0530

    added album update routes

--- modules/albums/album.routes.js
+++ modules/albums/album.routes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-const { createAblum } = require("./album.controller");
-const { validateCreateAlbum } = require("./album.validator");
+const { createAblum, updateAlbum } = require("./album.controller");
+const {
+  validateCreateAlbum,
+  validateAlbumId,
+  validateUpdateAlbum,
+} = require("./album.validator");
 
 router.post("/albums", validateCreateAlbum, createAblum);
+
+router.put(
+  "/albums/:albumId",
+  validateAlbumId,
+  validateUpdateAlbum,
+  updateAlbum,
+);
 
 module.exports = router;
