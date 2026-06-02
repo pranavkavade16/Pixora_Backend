9b66ca44b54ff40036334a8fa33dff2525a15384
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-02 23:57:49 +0530

    added email validator for shareing album access

--- modules/albums/album.validator.js
+++ modules/albums/album.validator.js
@@ -68,4 +68,44 @@ const validateUpdateAlbum = (req, res, next) => {
   next();
 };
 
-module.exports = { validateCreateAlbum, validateAlbumId, validateUpdateAlbum };
+const validateEmailForShareAlbum = (req, res, next) => {
+  const { emails } = req.body;
+
+  const errors = [];
+
+  if (!Array.isArray(emails)) {
+    errors.push("Emails must be an array.");
+  }
+
+  if (!emails || emails.length === 0) {
+    errors.push("At least one email is required.");
+  }
+
+  emails?.forEach((email) => {
+    if (
+      typeof email !== "string" ||
+      !email.includes("@") ||
+      email.startsWith("@") ||
+      email.endsWith("@")
+    ) {
+      errors.push(`${email} is not a valid email`);
+      return;
+    }
+  });
+
+  if (errors.length > 0) {
+    return res.status(400).json({
+      success: false,
+      errors,
+    });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateCreateAlbum,
+  validateAlbumId,
+  validateUpdateAlbum,
+  validateEmailForShareAlbum,
+};
