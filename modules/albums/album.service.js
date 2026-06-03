a6cdda1d0c90ad07194cee4944548ebb3952f5d6
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-03 10:18:01 +0530

    changes files name

--- modules/albums/album.service.js
+++ modules/albums/album.service.js
@@ -1,4 +1,5 @@
 const albumRepository = require("./album.respository");
+const 
 
 const createAlbum = async (data, userId) => {
   const existingAlbum = await albumRepository.findByNameAndOwner(
@@ -31,6 +32,28 @@ const updateAlbum = async (albumId, userId, description) => {
   }
 
   return await albumRepository.updateDescription(albumId, description);
+};
+
+const shareAlbum = async (albumId, userId, emails) => {
+  const album = await albumRepository.findById(albumId);
+
+  if (!album) {
+    throw new Error("Album not found");
+  }
+
+  if (album.ownerId.toString() !== userId) {
+    throw new Error("Only the owner can share this album");
+  }
+
+  const users = await userRepository.findByEmails(emails);
+
+  if (users.length !== emails.length) {
+    throw new Error("One or more users do not exist");
+  }
+
+  const userIds = users.map((user) => user._id);
+
+  return await albumRepository.addSharedUsers(albumId, userIds);
 };
 
 module.exports = { createAlbum };
