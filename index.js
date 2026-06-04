45ce8128a23d05850642ddd8772f6b4eab921945
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-04 19:15:38 +0530

    added index.js file and added routes to it

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { initializeDatabase } = require("./db/db.connect");
 
+const albumRoutes = require("./modules/albums/album.routes");
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/", albumRoutes);
