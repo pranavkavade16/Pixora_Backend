0a0f888c07a63880491a785c473013dd806727dd
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-05 11:07:35 +0530

    added user routes

--- modules/users/user.routes.js
+++ modules/users/user.routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {createUser} = require("./user.controller");
+const { createUser } = require("./user.controller");
+const { validateEmail } = require("./user.validator");
 
+// routes
+
+router.post("/users", validateEmail, createUser);
+
+module.exports = router;
