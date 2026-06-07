fa147ebd63d8114c47714deb75ba145ab0f7d101
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-07 19:32:35 +0530

    added fav image controller

--- modules/images/image.controller.js
+++ modules/images/image.controller.js
@@ -1,23 +1,36 @@
 const imageService = require("./image.service");
 
-exports.uploadImage = async (
-  req,
-  res,
-  next
-) => {
+exports.uploadImage = async (req, res, next) => {
   try {
-    const image =
-      await imageService.uploadImage(
-        req.params.albumId,
-        req.user.userId,
-        req.file,
-        req.body
-      );
+    const image = await imageService.uploadImage(
+      req.params.albumId,
+      req.user.userId,
+      req.file,
+      req.body,
+    );
 
     return res.status(201).json({
       success: true,
-      message:
-        "Image uploaded successfully",
+      message: "Image uploaded successfully",
+      data: image,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.favoriteImage = async (req, res, next) => {
+  try {
+    const image = await imageService.favoriteImage(
+      req.params.albumId,
+      req.params.imageId,
+      req.user.userId,
+      req.body.isFavorite,
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Favorite status updated successfully",
       data: image,
     });
   } catch (error) {
