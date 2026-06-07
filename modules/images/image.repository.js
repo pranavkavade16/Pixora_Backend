d90a26c48cabb9e3dcb0df7edf413e8864836c19
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-07 12:53:00 +0530

    added findby and update fav image repository

--- modules/images/image.repository.js
+++ modules/images/image.repository.js
@@ -3,3 +3,21 @@ const Image = require("./image.model");
 const upload = async (imageData) => {
   return await Image.create(imageData);
 };
+
+const findById = async (imageId) => {
+  return Image.findById(imageId);
+};
+
+const updateFavoriteStatus = async (imageId, isFavorite) => {
+  return Image.findByIdAndUpdate(
+    imageId,
+    {
+      isFavorite,
+    },
+    {
+      new: true,
+    },
+  );
+};
+
+module.exports = { upload, findById, updateFavoriteStatus };
