fe73301475b4436bccbed600e5d60991229e4d60
pranavkavade16 <pranavkavade69739@gmail.com>
2026-06-28 22:56:04 +0530

    fixed few errors in getLibrary api

--- modules/albums/album.service.js
+++ modules/albums/album.service.js
@@ -1,5 +1,6 @@
 const albumRepository = require("./album.respository");
 const userRepository = require("../users/user.repository");
+const Album = require("./album.model");
 
 const createAlbum = async (data, userId) => {
   const existingAlbum = await albumRepository.findByNameAndOwner(
