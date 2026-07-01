2426023c7aa52077dee95176ea7e6056a72c2940
pranavkavade16 <pranavkavade69739@gmail.com>
2026-07-01 23:35:26 +0530

    added getAllUsers api route

--- modules/users/user.routes.js
+++ modules/users/user.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { createUser } = require("./user.controller");
+const { createUser, getAllUsers } = require("./user.controller");
 const { validateEmail } = require("./user.validator");
 
 // routes
 
 router.post("/users", validateEmail, createUser);
 
+router.get("/users", getAllUsers);
 module.exports = router;
